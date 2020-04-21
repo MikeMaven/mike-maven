eb3a5d99c4572d77ff25e7f5aa7e5d01121d06e0
MikeMaven <mikemaven@gmail.com>
2020-04-21 14:55:41 -0400

    initial commit

---
+++ src/layout/MainFooter.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer
+    class="footer"
+    :class="{ [`footer-${type}`]: type }"
+    :data-background-color="backgroundColor"
+  >
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.mikemavendev.com">
+              Mike Maven
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/adogsdreamgame">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https:///www.instagram.com/adogsdreamgame">
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright">
+        &copy; {{ year }}, Designed by
+        <a href="https:///www.invisionapp.com" target="_blank" rel="noopener"
+          >Invision</a
+        >. Coded by
+        <a href="/" target="_blank" rel="noopener"
+          >Mike Maven</a
+        >.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  props: {
+    backgroundColor: String,
+    type: String
+  },
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
---
+++ src/layout/MainNavbar.vue
@@ -0,0 +1,137 @@
+<template>
+  <navbar
+    position="fixed"
+    type="primary"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+    <template slot-scope="{ toggle, isToggled }">
+      <router-link v-popover:popover1 class="navbar-brand" to="/">
+        Mike Maven
+      </router-link>
+      <el-popover
+        ref="popover1"
+        popper-class="popover"
+        placement="bottom"
+        width="200"
+        trigger="hover"
+      >
+        <div class="popover-body">
+          Expert Full Stack Web Developer &
+          Unity3D Developer
+        </div>
+      </el-popover>
+    </template>
+    <template slot="navbar-menu">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="mailto:mikemaven@gmail.com?subject=Tell%20me%20more."
+          target="_blank"
+        >
+          <i class="now-ui-icons tech_laptop"></i>
+          <p>Contact</p>
+        </a>
+      </li>
+      <drop-down
+        tag="li"
+        title="Web Development"
+        icon="now-ui-icons objects_globe"
+        class="nav-item"
+      >
+        <a 
+          href="https://github.com/mikemaven"
+          target="_blank"
+          class="dropdown-item"
+        >
+          <i class="now-ui-icons business_chart-pie-36"></i> GitHub
+        </a>
+        <a
+          href="https://demos.creative-tim.com/vue-now-ui-kit/documentation"
+          target="_blank"
+          class="dropdown-item"
+        >
+          <i class="now-ui-icons design_bullet-list-67"></i> Resume
+        </a>
+      </drop-down>
+      <drop-down
+              tag="li"
+              title="Game Development"
+              icon="now-ui-icons tech_controller-modern"
+              class="nav-item"
+      >
+        <nav-link to="/landing">
+          <i class="now-ui-icons location_compass-05"></i> A Dog's Dream
+        </nav-link>
+        <nav-link to="/login">
+          <i class="now-ui-icons users_circle-08"></i> Login
+        </nav-link>
+        <nav-link to="/profile">
+          <i class="now-ui-icons users_single-02"></i> Profile
+        </nav-link>
+      </drop-down>
+
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow Mike on Twitter"
+          data-placement="bottom"
+          href="https://twitter.com/FurlingForests"
+          target="_blank"
+        >
+          <i class="fab fa-twitter"></i>
+          <p class="d-lg-none d-xl-none">Twitter</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Like A Dog's Dream on Facebook"
+          data-placement="bottom"
+          href="https://www.facebook.com/adogsdreamgame"
+          target="_blank"
+        >
+          <i class="fab fa-facebook-square"></i>
+          <p class="d-lg-none d-xl-none">Facebook</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          rel="tooltip"
+          title="Follow us on Instagram"
+          data-placement="bottom"
+          href="https://www.instagram.com/adogsdreamgame"
+          target="_blank"
+        >
+          <i class="fab fa-instagram"></i>
+          <p class="d-lg-none d-xl-none">Instagram</p>
+        </a>
+      </li>
+    </template>
+  </navbar>
+</template>
+
+<script>
+import { DropDown, NavbarToggleButton, Navbar, NavLink } from '@/components';
+import { Popover } from 'element-ui';
+export default {
+  name: 'main-navbar',
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number
+  },
+  components: {
+    DropDown,
+    Navbar,
+    NavbarToggleButton,
+    NavLink,
+    [Popover.name]: Popover
+  }
+};
+</script>
+
+<style scoped></style>
